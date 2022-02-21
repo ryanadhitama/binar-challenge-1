1139f19f6174366c12d1cae4c6c413d5126e839f
ryanadhitama <ryan@vinceredigital.com>
2022-02-21 19:18:30 +0800

    update slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 AOS.init();
+
+const swiper = new Swiper(".swiper", {
+  // loop: true,
+  centeredSlides: true,
+  cssMode: true,
+  mousewheel: true,
+  keyboard: true,
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
